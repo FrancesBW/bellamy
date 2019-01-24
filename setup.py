b62cfc36e55dafdf2b9d112c5a96e6122e0a2fe8
FrancesBW <u5783737@anu.edu.au>
2019-01-24 10:39:03 +0800

    changed to package_data

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     url="https://github.com/FrancesBW/radio_cross_matching",
     long_description=['README.md'],
     packages=['functions'],
-    data_files=[('functions', 'functions/default_data/*.txt')],
+    package_data={'functions': ['default_data/*.txt']},
     install_requires=reqs,
     scripts=['scripts/maccas'],
     classifiers=['Programming Language :: Python :: 2.7']
