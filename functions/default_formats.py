629be4ae0b66e27809bd9ae6697709aeee77ef68
FrancesBW <u5783737@anu.edu.au>
2019-01-29 16:47:25 +0800

    support for aegean, TGSS, GLEAM

--- functions/default_formats.py
+++ functions/default_formats.py
@@ -1,1 +1,8 @@
 def GLEAM():
+      return ['RAJ2000','DEJ2000','err_RAJ2000','err_DEJ2000','psf_a','psf_a','a','b','pa','peak_flux','err_peak_flux','int_flux','local_rms','Name','76-227','suffix']  
+  
+def TGSS():
+      return ['RA','DEC','E_RA','E_DEC','','','Maj','Min','PA','Peak_flux','E_Peak_flux','Total_flux','','Source_name','150','']
+
+def target_default():
+      return ['ra','dec','err_ra','err_dec','psf_a','psf_b','a','b','pa','peak_flux','err_peak_flux','int_flux','local_rms','uuid']
