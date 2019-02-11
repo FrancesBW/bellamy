19c617dc7ecd1a8542b1af469d21885eab15487d
FrancesBW <u5783737@anu.edu.au>
2019-02-11 16:34:16 +0800

    Update matching.py

--- functions/matching.py
+++ functions/matching.py
@@ -334,13 +334,10 @@ def cross_matching(ref_catalogue, pre_snr_tar_catalogue, original_dist_tar_catal
                                 ref_cat_uuid.append(ref_uuid)
                                 tar_cat_uuid.append(tar_uuid)
 				
-	print('length: ',len(tar_cat_uuid))
-	print(tar_cat_uuid)
-        #tar_cat_uuid=np.array(tar_cat_uuid)                       
-	random.shuffle(tar_cat_uuid)		
+	copt=copy(tar_cat_uuid)                     
+	random.shuffle(tar_cat_uuid)	
 	
-	print('length: ',len(tar_cat_uuid))
-	print(tar_cat_uuid)
+	print('Are they still there?' ,np.all(np.isin(copt,tar_cat_uuid)))
 	
 	if len(cross_matched_table)>0 and final_run==False:
 		shift=0
