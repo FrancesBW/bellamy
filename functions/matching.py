31166e1ea408652eea58588eb8a2dde6ed41ebb4
FrancesBW <u5783737@anu.edu.au>
2019-02-11 15:57:12 +0800

    Update matching.py

--- functions/matching.py
+++ functions/matching.py
@@ -331,7 +331,7 @@ def cross_matching(ref_catalogue, pre_snr_tar_catalogue, original_dist_tar_catal
                                 ref_cat_uuid.append(ref_uuid)
                                 tar_cat_uuid.append(tar_uuid)
                                 
-	if len(cross_matched_table)>0:
+	if len(cross_matched_table)>0 and final_run==False:
 		rejected_match_idx=[]
 		for i in range(0,len(tar_cat_uuid)):
 			if reject_outliers(vstack([already_cross_matched,cross_matched_table]), tar_cat_uuid[i])=='reject':
